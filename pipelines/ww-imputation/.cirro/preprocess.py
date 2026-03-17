5c37f91794e8a913e022dfcd53fe60dac807c832
emjbishop <ebishop1024@gmail.com>
2026-03-17 13:20:56 -0700

    Initial commit of Cirro files for ww-imputation

---
+++ pipelines/ww-imputation/.cirro/preprocess.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+import json
+from cirro.helpers.preprocess_dataset import PreprocessDataset
+import pandas as pd
+
+
+def main():
+    # Get the information provided by the user
+    ds = PreprocessDataset.from_running()
+
+    # Set up the inputs JSON
+    setup_inputs(ds)
+    # Set up the options JSON
+    setup_options(ds)
+
+
+def write_json(fp, obj, indent=4) -> None:
+
+    with open(fp, "wt") as handle:
+        json.dump(obj, handle, indent=indent)
+
+
+def setup_inputs(ds: PreprocessDataset):
+    """
+    Format the inputs as JSON with the name "inputs.0.json"
+
+    Note: The workflow could be invoked multiple times by writing out multiple
+          inputs.*.json files. But for this scenario, only a single execution
+          is needed.
+    """
+
+    ds.logger.info("Formatting inputs")
+
+    inputs = {
+        "imputation.chromosomes": format_chromosomes(ds),
+        "imputation.input_crams": [v.strip() for v in ds.params["input_crams"].split(",")],
+        "imputation.input_cram_indices": [v.strip() for v in ds.params["input_cram_indices"].split(",")],
+        "imputation.reference_fasta": ds.params["reference_fasta"],
+        "imputation.reference_fasta_index": ds.params["reference_fasta_index"],
+        "imputation.output_prefix": ds.params["output_prefix"],
+        "imputation.output_format": ds.params["output_format"],
+        "imputation.chunk_cpu_cores": ds.params["chunk_cpu_cores"],
+        "imputation.chunk_memory_gb": ds.params["chunk_memory_gb"],
+        "imputation.phase_cpu_cores": ds.params["phase_cpu_cores"],
+        "imputation.phase_memory_gb": ds.params["phase_memory_gb"],
+        "imputation.ligate_cpu_cores": ds.params["ligate_cpu_cores"],
+        "imputation.ligate_memory_gb": ds.params["ligate_memory_gb"],
+        "imputation.concat_cpu_cores": ds.params["concat_cpu_cores"],
+        "imputation.concat_memory_gb": ds.params["concat_memory_gb"],
+    }
+    write_json("inputs.0.json", inputs)
+
+
+def format_chromosomes(ds: PreprocessDataset):
+    """
+    Format the chromosomes input using the ChromosomeData struct syntax:
+
+    [
+        {
+            "chromosome": "chr22",
+            "reference_vcf": "/path/to/1000GP_chr22.bcf",
+            "reference_vcf_index": "/path/to/1000GP_chr22.bcf.csi",
+            "genetic_map": "/path/to/genetic_map_chr22.txt"
+        },
+        ...
+    ]
+    """
+
+    # Get a table listing the reference panel files selected by the user.
+    # This DataFrame has columns: sample, file_1, file_2, file_3
+    df = ds.pivot_samplesheet()
+
+    chromosomes = []
+    for _, r in df.iterrows():
+        chromosomes.append({
+            "chromosome": r["sample"],
+            "reference_vcf": r["file_1"],
+            "reference_vcf_index": r["file_2"],
+            "genetic_map": r["file_3"],
+        })
+
+    return chromosomes
+
+
+def setup_options(ds: PreprocessDataset):
+    options = {
+        "workflow_failure_mode": "ContinueWhilePossible",
+        "write_to_cache": True,
+        "read_from_cache": True,
+        "default_runtime_attributes": {
+            "maxRetries": 1
+        },
+        "final_workflow_outputs_dir": ds.params["out_dir"],
+        "use_relative_output_paths": True
+    }
+    write_json("options.json", options)
+
+
+if __name__ == "__main__":
+    main()
